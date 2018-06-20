70f29c4351fe5b09a274b74d68e3827a6441ae53
Andrew Murphy <andrewmurphywastaken@gmail.com>
2018-06-20 12:59:07 -0400

    added firebase configuration

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+	apiKey: "AIzaSyB5w4RMIQuMTdOYoLeAAXep_00_uXGkH-I",
+	authDomain: "rps-online-78554.firebaseapp.com",
+	databaseURL: "https://rps-online-78554.firebaseio.com",
+	projectId: "rps-online-78554",
+	storageBucket: "",
+	messagingSenderId: "566981574178"
+};
+firebase.initializeApp(config);
